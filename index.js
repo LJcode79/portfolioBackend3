79459ef61700d6d685343cb8f0b5190d824f8f27
LJ <ljdb1999@gmail.com>
2025-05-01 22:47:44 -0700

    added contact route

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 const nodemailer = require("nodemailer");
 
 // server used to send send emails
 const app = express();
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './html/index.html'))
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./html/index.html"));
+});
 
-app.get('/hi',(req,res) => {
-    res.send('hiii')
-})
+app.get("/hi", (req, res) => {
+  res.send("hiii");
+});
 
-require('dotenv').config();
+require("dotenv").config();
 const contactEmail = nodemailer.createTransport({
-
-
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
     user: process.env.REACT_APP_EMAIL,
     pass: process.env.REACT_APP_EMAILPASSWORD,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
   }
 });
 
+app.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.REACT_APP_EMAIL,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
 
-app.listen(5000,()=> {
-    console.log('listening on 5000')
-})
+app.listen(5000, () => {
+  console.log("listening on 5000");
+});
