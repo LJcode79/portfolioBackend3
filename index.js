311739624c63f2ad5d39de7bfad808a786645d17
LJ <ljdb1999@gmail.com>
2025-05-01 23:53:40 -0700

    cors refactor 24

--- index.js
+++ index.js
@@ -16,6 +16,47 @@ app.get("/hi", (req, res) => {
   res.send("hiii");
 });
 
+require("dotenv").config();
+const contactEmail = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.REACT_APP_EMAIL,
+    pass: process.env.REACT_APP_EMAILPASSWORD,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+app.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.REACT_APP_EMAIL,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
 
 app.listen(5000, () => {
   console.log("listening on 5000");
