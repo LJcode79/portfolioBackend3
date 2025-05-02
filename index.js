22ab8753b49fbc58d22e0bfef1dad118b97c9a04
LJ <ljdb1999@gmail.com>
2025-05-02 00:09:08 -0700

    cors refactor 29

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ const cors = require("cors");
 const path = require("path");
 const nodemailer = require("nodemailer");
 
-
-app.use(cors());
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
 const app = express();
 
@@ -13,7 +30,6 @@ const app = express();
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./html/index.html"));
 });
-
 
 app.get("/hi", (req, res) => {
   res.send("hiii");
@@ -38,28 +54,32 @@ contactEmail.verify((error) => {
   }
 });
 
-app.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: process.env.REACT_APP_EMAIL,
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
+const contactPost = (req, res) => {
+  app.post("/contact", (req, res) => {
+    const name = req.body.firstName + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const phone = req.body.phone;
+    const mail = {
+      from: name,
+      to: process.env.REACT_APP_EMAIL,
+      subject: "Contact Form Submission - Portfolio",
+      html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
            <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json({ code: 200, status: "Message Sent" });
+      }
+    });
   });
-});
+};
+
+module.exports = allowCors(contactPost);
 
 app.listen(5000, () => {
   console.log("listening on 5000");
