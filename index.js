5bdfb4afb4409fd482f02ad0912b66206c902b18
LJ <ljdb1999@gmail.com>
2025-05-01 22:32:43 -0700

    comment out slash keep router

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const nodemailer = require("nodemailer");
 
 // server used to send send emails
 const app = express();
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './html/index.html'))
-  });
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './html/index.html'))
+//   });
 
 app.get('/hi',(req,res) => {
     res.send('hii')
 })
 
-app.listen(5000,()=> {
-    console.log('listening on 5000')
-})
+// app.listen(5000,()=> {
+//     console.log('listening on 5000')
+// })
 
 app.use("/", router);
 require('dotenv').config();
